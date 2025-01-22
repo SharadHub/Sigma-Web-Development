929c594a2239dc29a89567ee16283e63cd63d7b3
Sharad Bista <124153048+SharadHub@users.noreply.github.com>
2025-01-22 12:29:21 +0545

    Add files via upload

---
+++ javascript/first.js
@@ -0,0 +1,16 @@
+alert("hello world");
+
+console.log("code is running....!!!!!!");
+
+var a = prompt("enter your number");
+
+var isTrue = confirm("are you sure you want to leave this page");
+
+if(isTrue){
+    console.log("you are leaving page");
+}
+else{
+    console.log("you are not leaving page");
+}
+
+console.log("your number is"+ a)
---
+++ javascript/script.js
@@ -0,0 +1,1 @@
+alert("this is sigma course for web development");
